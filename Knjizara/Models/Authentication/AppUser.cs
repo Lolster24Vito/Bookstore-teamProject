ce9cfd613e6cc6d5c16ff2845f6cdcdd04b4eadc
Lolster24Vito <vitomir.hardi@gmail.com>
2022-05-27 14:47:05 +0200

    Added a basic user registration and login

---
+++ Knjizara/Models/Authentication/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Knjizara.Models.Authentication
+{
+    public class AppUser:IdentityUser<Guid>
+    {
+        [Display(Name ="Ime")]
+        [StringLength(250)]
+        [Required(ErrorMessage ="Ovo polje je obavezno")]
+        public string FirstName { get; set; }
+        [Display(Name = "Prezime")]
+        [Required(ErrorMessage = "Ovo polje je obavezno")]
+        [StringLength(250)]
+
+        public string LastName { get; set; }
+        [Display(Name = "Adresa")]
+        [StringLength(350)]
+        public string? Address { get; set; }
+
+
+    }
+}
