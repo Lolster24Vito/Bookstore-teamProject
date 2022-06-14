454c09e3c12660eec450b8f380ddd872c6921fb2
Lolster24Vito <vitomir.hardi@gmail.com>
2022-06-15 01:57:52 +0200

    Polished code

---
+++ Knjizara/Models/ViewModels/CollectionBookAuthor.cs
@@ -0,0 +1,18 @@
+﻿using Knjizara.Models.BaseEntities;
+using Knjizara.Models.Books;
+
+namespace Knjizara.Models.ViewModels
+{
+    public class CollectionBookAuthor
+    {
+        public Author Author { get; set; }
+        public Book Book { get; set; }
+
+
+        public CollectionBookAuthor(Book book,Author author)
+        {
+            Book = book;  
+            Author = author;
+        }
+    }
+}
