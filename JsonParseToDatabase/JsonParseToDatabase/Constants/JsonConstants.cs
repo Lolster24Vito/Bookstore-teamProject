831bb2fbc4800ebd090b1a5f6e6a754a02f8ed3a
Dominik Arapov <darapov@racunarstvo.hr>
2022-09-09 00:03:24 +0200

    Frontend

--- JsonParseToDatabase/JsonParseToDatabase/Constants/JsonConstants.cs
+++ JsonParseToDatabase/JsonParseToDatabase/Constants/JsonConstants.cs
@@ -8,6 +8,7 @@ namespace JsonParseToDatabase.Constants
 {
     public static class JsonConstants
     {
-        public const string BOOKS = @"C:\Users\Leo\source\repos\PRAKnjizara\Knjizara\BooksJson\PiK.json";
+        //public const string BOOKS = @"C:\Users\Leo\source\repos\PRAKnjizara\Knjizara\BooksJson\PiK.json";
+        public const string BOOKS = @"C:\Users\DominikArapov\PRA_KNJIZARA\Knjizara\BooksJson\PiK.json";
     }
 }
